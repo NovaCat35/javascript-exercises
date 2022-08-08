05b87e0ffeb82c89740059711b571b6c806dcf0e
Robert Chen <bergintonatcapital@gmail.com>
2022-08-08 12:11:40 -0400

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    } 
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return 'ERROR';
+    }
 
+    let total = 0;
+    let startNum = Math.min(num1, num2);
+    let endNum = Math.max(num1, num2);
+    
+    for(let i = startNum; i <= endNum; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
