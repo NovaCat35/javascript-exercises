4105a72c5b4e2a79d93b794dea7c6f9f2613c132
Robert Chen <bergintonatcapital@gmail.com>
2022-08-08 11:48:40 -0400

    Finish 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray, ...theArgs) {
+    let oldArray = Array.from(arguments[0]);
+    let restParaArray = theArgs;
+    let newArray = [];
+
+    nextElement:
+    for(let i = 0; i < oldArray.length; i++) {
+        for(let k = 0; k < restParaArray.length; k++) {
+            if (oldArray[i] === restParaArray[k]) {
+                continue nextElement;
+            }
+        }
+        newArray.push(oldArray[i]);
+    }
+    return newArray;
 
 };
 
