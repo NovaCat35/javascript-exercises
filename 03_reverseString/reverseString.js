c180087a714b3bacb6a9c2c951af59927b57b243
Robert Chen <bergintonatcapital@gmail.com>
2022-08-08 00:44:11 -0400

    Finish 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const strToArray = str.split('');
+    let reverseStr = ""
 
+    for(let i = str.length-1; i >= 0 ; i--) {
+        reverseStr += strToArray[i];
+    }
+    return reverseStr;
 };
 
 // Do not edit below this line
