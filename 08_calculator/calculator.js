e7ac0693f59b47af8759dfc65ea7324ec1e31d88
Robert Chen <bergintonatcapital@gmail.com>
2022-12-29 21:22:21 -0500

    Add solution 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numList) {
+	return numList.reduce((accumulator, num) => accumulator + num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numList) {
+	return numList.reduce((accumulator, num) => accumulator * num, 1)
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return Math.pow(num, power)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1
+  for(let i = num; i > 0; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
+// This is another implementation of Factorial that uses recursion
+// const recursiveFactorial = function(n) {
+//   if (n === 0) {
+//     return 1;
+//   }
+//   return n * recursiveFactorial (n-1);
+// };
 
 // Do not edit below this line
 module.exports = {
