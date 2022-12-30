a6f175cedf247c9805195af45a2b0ade3613bd26
Robert Chen <bergintonatcapital@gmail.com>
2022-12-30 12:32:43 -0500

    Finish 09 and 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z]/g, '');
 
+    while(string.length > 1) {
+        if(string[0] === string[string.length-1]){
+            string = string.slice(1,string.length-1);
+            continue;
+        }
+        return false;
+    }
+    return true;
+
+    // ~~ SOLUTION ~~
+    // return (
+    //     string
+    //       .split("")
+    //       .reverse()
+    //       .join("") == string
+    // );
 };
 
 // Do not edit below this line
